e1ea673fb03d3381db78e929a11f0f5795c93827
이은솔 <lucinda96@naver.com>
2024-05-08 09:02:19 +0900

    remove src folder

--- src/main/java/com/justdo/climbing/constant/GradeEnum.java
+++
@@ -1,7 +0,0 @@
-package com.justdo.climbing;
-
-public enum GradeEnum {
-    A_GRADE,
-    B_GRADE,
-    C_GRADE
-}
--- src/main/java/com/justdo/climbing/model/constant/GradeEnum.java
+++
@@ -1,7 +0,0 @@
-package com.justdo.climbing.model.constant;
-
-public enum GradeEnum {
-    A_GRADE,
-    B_GRADE,
-    C_GRADE
-}
